60cc489cb0ae53f51ed12cf4dd29ae0b1ed66517
Mr.Xbott <mr.xbott@gmail.com>
2022-12-07 13:57:51 +0300

    tests updated

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,16 +1,9 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-
-client = TestClient(app)
-
-def test_get_categories():
+def test_get_categories(client):
     response = client.get('/api/v1/categories/')
     assert response.status_code == 200
     assert response.json()
 
-def test_get_category_by_id():
+def test_get_category_by_id(client):
     category_id = 1
     category_name = 'Pasta & Macaroni'
 
@@ -20,13 +13,13 @@ def test_get_category_by_id():
     assert response.json()['category'] == category_name
     # to do: check with products in the category
 
-def test_get_category_by_id_not_found():
+def test_get_category_by_id_not_found(client):
     category_id = 0
     response = client.get(f'/api/v1/categories/{category_id}')
     assert response.status_code == 404
     assert response.json() == {'detail': 'Not Found'}
 
-def test_get_category_by_id_wrong_type():
+def test_get_category_by_id_wrong_type(client):
     category_id = 'a'
     response = client.get(f'/api/v1/categories/{category_id}')
     assert response.status_code == 422
